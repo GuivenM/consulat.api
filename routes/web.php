91b223acbf30dfadb73a5e048e4bef1a74c6f00e
GuivenM <guivenpambou@gmail.com>
2026-09-23 07:44:41 +0100

    Retrait de la route storage custom, retour au mécanisme standard Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-use Illuminate\Support\Facades\Storage;
-
-Route::get('/storage/{path}', function ($path) {
-    $fullPath = storage_path('app/public/' . $path);
-    
-    if (!file_exists($fullPath)) {
-        abort(404);
-    }
-    
-    return response()->file($fullPath, [
-        'Access-Control-Allow-Origin' => '*',
-        'Access-Control-Allow-Methods' => 'GET',
-        'Access-Control-Allow-Headers' => 'Origin, Content-Type, Accept, Authorization',
-    ]);
-})->where('path', '.*');
